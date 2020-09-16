04f6ebec9868ca6913209e2173ef04b47f34f877
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-16 17:00:28 +0000

    adding file to directory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *Write a C program that prints exactly
+ *Use the function printf
+ */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-        return 0;
+        return (0);
 }

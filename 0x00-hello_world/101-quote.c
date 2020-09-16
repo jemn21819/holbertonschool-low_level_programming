e66c3934e427d5f068995b04d53019aba87d3609
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-16 23:14:19 +0000

    adding file to directory

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Print etxactly
+ *
+ *Return: always 0 (succes)
+ */
+in main(void)
+{
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+return (1);
+}

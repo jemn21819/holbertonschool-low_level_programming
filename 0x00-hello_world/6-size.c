0fcbce46ff43f89bfd140b2565cae62ae972a7ca
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-16 20:05:10 +0000

    adding file to directory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Print the different data type
- * Description: Print size of char, float, long long, int, long int
+ * main - Print the size of data
+ * Description: Print size of the data requested
  * Return: 0
-*/
+ */
 int main(void)
 {
-printf("Size of a char: %u byte(s)\n", sizeof(char));
-printf("Size of a int: %u byte(s)\n", sizeof(int));
-printf("Size of a long int: %u byte(s)\n", sizeof(long));
-printf("Size of a long long: %u byte(s)\n", sizeof(long long));
-printf("Size of a float: %u byte(s)\n", sizeof(float));
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of a int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long));
+printf("Size of a long long: %i byte(s)\n", sizeof(long long));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
 }

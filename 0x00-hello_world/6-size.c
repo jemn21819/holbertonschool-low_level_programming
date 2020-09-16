0c2c3975dc369aea59799d71e8c555265ef85777
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-16 21:40:24 +0000

    adding file to directory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Print the size of data
- * 
+ *
  * Return: 0
  */
 int main(void)
 {
-  printf("Size of a char: %ld byte(s)\n", sizeof(char));
-  printf("Size of an int: %ld byte(s)\n", sizeof(int));
-  printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-  printf("Size of a long long: %ld byte(s)\n", sizeof(long long));
-  printf("Size of a float: %ld byte(s)\n", sizeof(float));
-  return (0);
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long: %ld byte(s)\n", sizeof(long long));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
 }

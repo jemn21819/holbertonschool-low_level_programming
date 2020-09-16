9761aee2ea81930b8fa193e90941530c26271ad7
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-16 18:31:05 +0000

    adding file to directory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *Write a C program that prints exactly "Programming is like building a multiligual puzzle, followed by a new line.
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return 0;
+}

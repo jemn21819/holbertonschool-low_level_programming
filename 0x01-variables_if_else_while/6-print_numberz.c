0168b5aafa244b3453c59178d46759b44a94d55e
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-17 19:26:03 +0000

    adding directory

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Write num with base 10 wih putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar('%d', x);
+	}
+	putchar('\n');
+
+	return (0);
+}

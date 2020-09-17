125360918086d1016219a48910cd3416dc3e68bb
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-17 00:30:13 +0000

    adding file to directory

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - Print etxactly
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 63)
 return (1);
 }

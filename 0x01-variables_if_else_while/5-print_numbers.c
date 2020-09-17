35c227091a1844202c003a624d77f4be4d775993
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-17 19:09:55 +0000

    adding directory

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Pritn alphabet in lowcase
+ *
+ * Return:  Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = o; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+
+	return (0);
+}

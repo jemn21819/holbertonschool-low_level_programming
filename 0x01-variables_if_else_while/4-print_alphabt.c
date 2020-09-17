4c6b3d659b8d552d90a561e374bc91f7c0606b67
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-17 18:57:34 +0000

    adding directory

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Pritn alphabet in lowcase except q and e
+ *
+ * Return:  Always 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; 'a' <= 'z'; x++)
+	{
+		if (x == 'q' || x == 'e')
+			continue;
+	putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}

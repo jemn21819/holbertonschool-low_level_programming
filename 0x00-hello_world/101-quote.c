d2c32200f397f70265a930e10f547586c1037132
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-17 00:38:49 +0000

    adding file to directory

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-echo("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 63);
 return (1);
 }

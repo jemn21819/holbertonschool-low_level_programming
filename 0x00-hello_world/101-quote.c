63c7163a67ea4ffcc764fbb82ec439016c3ed128
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-17 00:14:53 +0000

    adding file to directory

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 in main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
-return (1);
+  echo("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+    return (1);
 }

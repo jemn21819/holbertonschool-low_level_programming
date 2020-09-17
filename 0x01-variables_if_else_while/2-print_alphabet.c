282fc5b753e9a4c708ee023b400c2f2b5b79af66
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-17 17:58:05 +0000

    adding directory

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Pritn alphabet in lowcase
+ *
+ * Return:  Always 0 (success)
+*/
+
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}

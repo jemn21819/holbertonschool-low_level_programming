95909f38ad4435bb77a0a84092ecaf7321ab3b16
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-18 02:58:11 +0000

    adding directory

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,16 +9,20 @@
 int main(void)
 {
 	int x;
+	int y;
 
-	for (x = '00'; x <= '99'; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		putchar(x);
-		if (x != '99')
+		for (y = '0'; y <= '9'; y++)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(x);
+			putchar(y);
+			if (x != '9' || y != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-	}
 	putchar('\n');
 	return (0);
 }

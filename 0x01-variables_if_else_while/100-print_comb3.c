69e510483c4e3ef55d064a3743f7316795f462d0
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-18 03:16:03 +0000

    adding directory

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			if (y == '0')
+				y = y + x + 1;
+			putchar(x);
+			putchar(y);
+			if (x != '8' || y != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

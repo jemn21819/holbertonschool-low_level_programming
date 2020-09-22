797d150c2f4aa79804f5d502f5fa5c7068d495af
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-22 16:36:07 +0000

    adding file to directory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adding two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: Sum of x + y
+ */
+
+int add(int x, int y);
+{
+	return (x + y);
+}

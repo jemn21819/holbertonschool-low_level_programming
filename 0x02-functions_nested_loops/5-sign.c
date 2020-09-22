92a33b1464e16836df0e9fcc6b4890247204c7cb
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-22 11:27:43 +0000

    adding file to directory

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,22 @@ int print_sign(int n)
 	if (n > 48)
 	{
 		_putchar(43);
+		_putchar(44);
+		_putchar(32);
 		return (1);
 	}
 	else if (n == 48)
 	{
 		_putchar(48);
+		_putchar(44);
+		_putchar(32);
 		return (0);
 	}
 	else
 	{
 		_putchar(45);
+		_putchar(44);
+		_putchar(32);
 		return (-1);
 	}
 }

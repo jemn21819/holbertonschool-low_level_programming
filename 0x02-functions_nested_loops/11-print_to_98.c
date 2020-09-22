a52b4cd240300c1f9315471850e6615367401e2a
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-22 17:01:00 +0000

    adding file to directory

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,15 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
-	{
-		for (; n < 98; n++)
-			printf("%i, ", n);
-	}
 	if (n > 98)
 	{
 		for (; n > 98; n--)
-			printf("%i, ", n);
+			printf("%d, ", n);
+	}
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+			printf("%d, ", n);
 	}
 	printf("98\n");
 }

0daa2fec22c14645f1e66f3b2c3fdee8a5ddb120
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-22 11:38:06 +0000

    adding file to directory

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,17 +3,17 @@
 /**
  * print_sign - print the sing of a int
  * @n: is a int
- * Return: 0 for zero, 1 for positive and -1 for negative
+ * Return: 1 for positive, 0 for zero, -1 for negative
  */
 
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 48)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);

419bbfde889b07c161d63170810d9c7f503b65bc
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-22 16:10:51 +0000

    adding file to directory

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * times_table - Print the 9 table
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			c + x * y;
+			_putchar(',');
+			_putchar(' ');
+			if (c >= 10)
+			{
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}

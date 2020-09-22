7361ccea4ae05d33a1a1a12d0ec9388a190db512
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-22 19:03:01 +0000

    Adding file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > c)
 	{
 		largest = b;
 	}
-	else if (a == b)
-	{
-		largest = a;
 	else
 	{
 		largest = c;

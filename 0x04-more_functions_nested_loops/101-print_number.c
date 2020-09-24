46d581c07c93c1ed94e69cbef134a07a45548ea7
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 19:25:17 +0000

    adding file to directory

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,21 @@
 
 void print_number(int n)
 {
+	int x;
+
 	if (n < 0)
 	{
-		n = -n;
+		x = -n;
 		_putchar('-');
 	}
 	else
 	{
-		n = n;
+		x = n;
 	}
-	if (n / 10)
+	if (x / 10)
 	{
-		print_number(n / 10);
+		print_number(x / 10);
 	}
 
-	_putchar(n % 10 + '0');
+	_putchar(x % 10 + '0');
 }

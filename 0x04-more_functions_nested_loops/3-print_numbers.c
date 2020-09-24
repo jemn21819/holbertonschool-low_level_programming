62e8a289309610b3b88f6ca3491b503de5719dad
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 16:55:14 +0000

    adding file to directory

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,15 @@
 
 /**
  * print_numbers - rints the numbers, from 0 to 9
- *
- * Return: Always 0
  */
 
 void print_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 48; x <= 57; x++)
 	{
 		_putchar(x);
 	}
 	_putchar('\n');
-	return (0);
 }

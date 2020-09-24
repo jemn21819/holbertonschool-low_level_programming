bdf202ee36b45d7ca606aedca2859bbe82095bb2
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 16:58:01 +0000

    adding file to directory

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,6 @@
 
 /**
  * print_most_numbers - prints the numbers, from 0 to 9 except 2 and 4
- *
- * Return: Always 0
  */
 
 void print_most_numbers(void)
@@ -13,10 +11,8 @@ void print_most_numbers(void)
 
 	for (x = 48; x <= 57; x++)
 	{
-		if (x == 50 && x == 52)
-			continue;
+		if (x != 50 && x != 52)
 		_putchar(x);
 	}
 	_putchar('\n');
-	return (0);
 }

e842154124def95794bc2ab8d8a174f5b8a1a307
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 17:40:21 +0000

    adding file to directory

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print fizz num/3, buzz num/5, fizzbuzz num/ 3 & 5
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 15 == 0)
+			printf("FizzBuzz ");
+		else if (x % 3 == 0)
+			printf("Fizz ");
+		else if (x % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%i ", x);
+	}
+	return (0);
+}

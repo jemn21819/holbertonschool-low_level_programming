ce0ae082bf15e122f643b888706606ccb6d53e7b
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 11:58:05 +0000

    Adding directory

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,35 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* 0. isupper */
+int _putchar(char);
+
+/* 1. isdigit */
+int _isdigit(int);
+
+/* 2-mul.c */
+int mul(int a, int b);
+
+/* 3-print_numbers.c */
+void print_numbers(void);
+
+/* 4-print_most_numbers.c */
+void print_most_numbers(void);
+
+/* 5-more_numbers.c */
+void more_numbers(void);
+
+/* 6-print_line.c */
+void print_line(int);
+
+/* 7-print_diagonal.c */
+void print_diagonal(int);
+
+/* 8-print_square.c */
+void print_square(int size);
+
+/* 9-fizz_buzz.c */
+
+
+/* 10-print_triangle.c */
+void print_triangle(int size);

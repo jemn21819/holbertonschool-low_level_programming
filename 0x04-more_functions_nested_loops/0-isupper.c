b1726599e4c8d6836d8841142c6f0968695b3ad3
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 12:56:50 +0000

    adding file to directory

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isupper - Checks for uppercase
+ * @c: Integer
+ * Return: 1 if is upper, 0 otherwise
+ */
+
+int _isupper(int c);
+{
+
+	if (c >= 'A' &&  c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

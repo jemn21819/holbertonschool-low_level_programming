5ee277b7a247eee8c1d0255e9cf5cf5c783ecd2e
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 18:13:30 +0000

    adding file to directory

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (x % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (x % 5 == 0)
-			printf("Buzz ");
-		else if (x == 100)
 			printf("Buzz");
 		else
-			printf("%d ", x);
+			printf("%d", x);
+		if (x < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);

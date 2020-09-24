45ca245dfe75f1e0bba09cae73ab92cba8ddbe06
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 13:13:25 +0000

    adding file to directory

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#inclidue "holberton.h"
+
+/**
+ * _isdigit - Check for a digit 0 - 9
+ * @c: Integer
+ * Return: 1 for a digit, 0 otherwise
+ */
+
+int _isdigit(int c);
+{
+
+	if (c >= 48 && <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

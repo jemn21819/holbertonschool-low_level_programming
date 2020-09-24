518fba4ffda52f4db8eb78b0742cd17d2b866d48
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-24 17:10:59 +0000

    adding file to directory

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 9 x++)
+	for (x = 0; x < 10 x++)
 	{
-		for (y = 0; y <= 14; y++)
+		for (y = 0; y < 15; y++)
 		{
 			if (y >= 10)
 				_putchar(y / 10 + '0');

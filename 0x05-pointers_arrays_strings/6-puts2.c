015616457c4d066f85cf67fa3bb54d2cb9162075
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-29 18:54:12 +0000

    Adding file to directory

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with first char
+ * @str: String
+ */
+
+void puts2(char *str)
+{
+	int x = 0;
+
+	while (*(str + x) != '\0')
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(*(str + x));
+		}
+		x++;
+	}
+	_putchar('\n');
+}

62415345fa7b2a48f1e4cd5389c8c8f898faa575
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-29 14:36:20 +0000

    Adding file to directory

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,12 @@
-#incude "holberton.h>
+#incude "holberton.h"
 
 /**
 * reset_to_98 - ointer to an int as parameter and updates the value it
 * points to to 98
-*
+* @n: Pinter
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

8e22fc294d966cb998661f320349c9711fd9a7bb
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-09-29 20:40:38 +0000

    Adding file to directory

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,12 @@ void puts_half(char *str)
 {
 	int x = 0;
 	int y;
-	int z = (x % 2) == 0 ? (x / 2) : (x / 2);
 
 	while (*(str + x) != '\0')
 	{
 		x++;
 	}
-	for (y = z + 1; y < x; y++)
+	for (y = (x % 2) == 0 ? (x / 2) : (x / 2) + 1; y < x; y++)
 	{
 		_putchar(*(str + y));
 	}

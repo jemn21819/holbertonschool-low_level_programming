0d7e792f79c500efd0e6aa07fcef6e3829046143
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-01 22:28:32 +0000

    Adding new file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - function that capitalizes all words of a string
+ * @s: String
+ * Return: s
+ */
+
+char *cap_string(char *s)
+{
+	int x = 0;
+	int y = 0;
+	char a[] = {32, '\t', '\n', 43, 33, 34, 40, 41, 59, 63, 46, 123, 125};
+
+	while (s[x] != '\0')
+	{
+		x++;
+		if (s[x] >= 'a' && s[x] <= 'z')
+		{
+			if (x == 0)
+				s[x] -= 32;
+			while (s[x] != '\0')
+			{
+				x++;
+				if (a[x] == s[x - 1])
+				{
+					s[x] -= 32;
+					break;
+				}
+			}
+		}
+	}
+	return (s);
+}

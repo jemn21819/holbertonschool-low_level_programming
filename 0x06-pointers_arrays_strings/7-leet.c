39fe18dde73119c17a6714e6e8483b253934bb2f
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-01 23:59:48 +0000

    Adding new file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * leet - function that encodes a string into 1337.
+ * @s: string
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	int x = 0;
+	int y;
+
+	char leet[10] = "aAeEoOtTlL";
+	char num[10] = "4433007711";
+
+	while (s[x] != '0')
+	{
+		x++;
+		for (y = 0; y < 10; y++)
+		{
+			if (s[x] == leet[y])
+			{
+				s[x] = num[y];
+			}
+		}
+	}
+	return (s);
+}

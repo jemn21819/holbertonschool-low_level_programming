2eead03c229bceed8fea474839b1e0e2c494b01c
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-01 15:21:48 +0000

    Adding new file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: first string
+ * @src: second string
+ * Return: Ponter dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x;
+	int y;
+
+	for (x = 0; dest[x]; x++)
+		;
+	for (y = 0; src[y]; y++)
+	{
+		dest[x] = src[y];
+		x++;
+	}
+	dest[c + 1] = '\0';
+	return (dest);
+}

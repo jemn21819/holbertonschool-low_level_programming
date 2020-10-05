44adb8aa9d8cce4906aaa52705c90d71cadd0ef6
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-05 21:47:41 +0000

    Adding files to directory

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
+		_putchar('\n');
+	}
+}

5fb24d6746b24a7de537cdd4e18c59977b300046
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-05 18:59:00 +0000

    Adding files to directory

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,19 +14,18 @@ unsigned int _strspn(char *s, char *accept)
 	int x = 0;
 	int y, z;
 
-	while (s[x] != '\0')
+	for (x = 0; s[x]; x++)
 	{
-		x++;
-	}
-	for (y = 0; accept[y] >= 0; y++)
-	{
-		if (s[x] == accept[y])
+		for (y = 0; accept[y] >= 0; y++)
 		{
-			res++;
-			break;
+			if (s[x] == accept[y])
+			{
+				res++;
+				break;
+			}
+			if (accept[y] == '\0')
+				return (res);
 		}
-		if (accept[y] == '\0')
-			return (res);
 	}
 	return (res);
 }

7bf32fcfa168a4ffc9cbad69d5ed3811bcba597c
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-05 17:44:19 +0000

    Adding files to directory

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _strchr - function that locates a character in a string.
+ * *_strchr - function that locates a character in a string.
  * @s: string
  * @c: character
  * Return: char or null
@@ -14,7 +14,7 @@ char *_strchr(char *s, char c)
 	for (x = 0; s[x]; x++)
 	{
 		if (s[x] == c)
-		return (s + x);
+			return (s + x);
 	}
 	if (c == '\0')
 	{

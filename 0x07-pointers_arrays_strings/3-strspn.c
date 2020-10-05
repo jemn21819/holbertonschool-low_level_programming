26774f7613aa89d211fb7e2ce4ec6bb4a6e1d889
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-05 18:44:55 +0000

    Adding files to directory

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: string
+ * @accept: bytes accepted
+ * Return: Number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int res = 0;
+
+	int x = 0;
+	int y, z;
+
+	while (s[x] != '\0')
+	{
+		x++;
+		for (y = 0; accept[y] >= 0; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				ress++;
+				break;
+			}
+			if (accept[y] == '\0')
+				return (res);
+		}
+	}
+	return (res);
+}

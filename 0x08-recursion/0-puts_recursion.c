5fe7b1ba4f3ddd600b8c3b63df5cd90369f60789
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-06 20:53:37 +0000

    Adding directory

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ *
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_putchar(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

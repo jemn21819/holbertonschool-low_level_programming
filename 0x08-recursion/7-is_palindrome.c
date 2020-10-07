96ae202c1cf5039e6fc4868172c860af06ea96fe
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-07 17:53:00 +0000

    adding files to directory

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - unction that returns the length of a string.
+ * @s: string
+ * Return: string length
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
+/**
+ * _palin - palindrome funcion
+ * @s: string
+ * @p: pointer
+ * Return: 1 for true 0 for false
+ */
+
+int _palin(char *s, char *p)
+{
+	if (*s == *p)
+		return (_palin(s + 1, p - 1));
+	if (s > p)
+		return (1);
+	else
+		return (0);
+}
+
+
+/**
+ * is_palindrome - function that returns 1 if string is a palindrome and 0 not
+ * @s: string
+ * Return: 1 if true 0 if false
+ */
+
+int is_palindrome(char *s)
+{
+	int strLen = _strlen_recursion(s) - 1;
+
+	return (_palin(s, 0, strLen));
+}

d928da619bb6a62cbd2cae8c09f3e2b3845fd7ce
jemn21819 <jmartinez21734@stu.nuc.edu>
2020-10-07 14:28:59 +0000

    adding files to directory

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - unction that returns the length of a string.
+ * @s: string
+ * Return: string length
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}

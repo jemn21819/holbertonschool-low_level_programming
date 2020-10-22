2e83bc06835cb615a6ac151493d9b96557af9a90
Jaime Martinez <jemn1jemn@gmail.com>
2020-10-22 02:05:08 +0000

    adding files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,9 +18,9 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	int i = 0;
 
-	while (i != 5)
+	while (ops[1].op != NULL)
 	{
-		if (!strcmp(s, ops[i].op))
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}

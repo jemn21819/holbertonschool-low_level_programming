0a1fa62e349d416cc69feb86c4b4fc4587e25dfb
Jaime Martinez <jemn1jemn@gmail.com>
2020-10-22 03:41:42 +0000

    adding files

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,16 +9,12 @@
 
 int main(int argc, char *argv[])
 {
-	int i, num;
-	char *ptr = (char *)main;
+	int num;
 
 	if (argc != 2)
 		printf("Error\n"), exit(1);
 	num = atoi(argv[1]);
 	if (num < 0)
 		printf("Error\n"), exit(2);
-	for (i = 0; i < num - 1; i++)
-		printf("%02hhx\n", ptr[i]);
-	printf("%02hhx\n", ptr[i]);
 	return (0);
 }

fadc202e2454e136215519da3848561e217dad60
Jaime Martinez <jemn1jemn@gmail.com>
2020-10-29 00:42:08 +0000

    adding file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * hare_tortoise - function that prints 
+ * */
+
+void hare_tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

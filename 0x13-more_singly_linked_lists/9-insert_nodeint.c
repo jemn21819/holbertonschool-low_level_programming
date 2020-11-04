d29ceee9e06d9b053a1974d8904644ed4e2b4dc6
Jaime Martinez <jemn1jemn@gmail.com>
2020-11-04 00:51:50 +0000

    adding file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *actual;
 	listint_t *new_node;
 
-	unsigned int i;
+	uint i;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!head || !new_node)

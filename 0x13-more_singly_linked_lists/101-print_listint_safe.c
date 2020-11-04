6364bcd6b09ce7f9878527526b4596ebf488bf8c
Jaime Martinez <jemn1jemn@gmail.com>
2020-11-04 16:08:00 +0000

    adding files

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to head
+ * Return: number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int node;
+	int count = 0;
+
+	while (head)
+	{
+		node = head - head->next;
+		count++;
+		printf("[%p] %d\n", (void *)head, head->n);
+
+		if (node > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+
+	}
+	return (count);
+}

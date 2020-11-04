4a41283a47d079954bc1ac1bb2da55c47ba7cac8
Jaime Martinez <jemn1jemn@gmail.com>
2020-11-04 16:09:01 +0000

    adding files

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,13 +15,13 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		node = head - head->next;
 		count++;
-		printf("[%p] %d\n", (void *)head, head->n);
+		printf("[%p] %i\n", (void *)head, head->n);
 
 		if (node > 0)
 			head = head->next;
 		else
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %i\n", (void *)head->next, head->next->n);
 			break;
 		}
 

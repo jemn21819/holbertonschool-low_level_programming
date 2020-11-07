f7eae0069eb1022aa3544dd481386d56d8454186
Jaime Martinez <jemn1jemn@gmail.com>
2020-11-07 01:31:50 +0000

    adding new files

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,13 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = '0'; x <= '8'; x++)
+	for (x = 48; x < 58; x++)
 	{
-		for (y = '0'; y <= '9'; y++)
+		for (y = x + 1; y < 58; y++)
 		{
-			if (y == '0')
-				y = y + x + 1;
 			putchar(x);
 			putchar(y);
-			if (x != '8' || y != '9')
+			if (x != 56)
 			{
 				putchar(',');
 				putchar(' ');
@@ -29,3 +27,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
